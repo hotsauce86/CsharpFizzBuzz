f5bf01748ee6b1c35981613c82cfb357dc1f3a6f
hotsauce86 <timothy_tierney@my.uri.edu>
2017-04-02 18:32:32 -0400

    Main Code
    added fizz buzz

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//USING C#
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else
+                    Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
